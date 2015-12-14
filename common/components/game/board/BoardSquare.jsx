1e83d6d748b6d7e27e001dab64376f1e663a4e33
Joshua Comeau <joshwcomeau@gmail.com>
2015-12-13 21:43:08 -0500

    Implemented DnD! Turns out 99% of the logic was already done from the Meteor port, just had to move the data around. Woot.

--- common/components/game/board/BoardSquare.jsx
+++ common/components/game/board/BoardSquare.jsx
@@ -10,28 +10,11 @@ const BoardSquare = React.createClass({
     y:      PropTypes.number.isRequired,
     isOver: PropTypes.bool.isRequired
   },
-  clickHandler() {
-    // TEMP: For now, we're going to pretend that clicking a tile is the
-    // same as dropping the first rack tile onto it. Just to practice.
-    let tileId = __store.getState().toJS().game.rack[0]._id;
-    console.log("Got tile", tileId);
-
-    const tileData = {
-      _id: tileId,
-      x: this.props.x,
-      y: this.props.y,
-      location: 'board'
-    };
-
-    // Invoke the action.
-    this.props.placeTile(tileData);
-
-  },
   render() {
     const { connectDropTarget, isOver } = this.props;
 
     return connectDropTarget(
-      <div className="board-square" onClick={this.clickHandler}>
+      <div className="board-square">
         { this.props.children ? <Tile tile={this.props.children} /> : null }
         { isOver ? <div className='square-overlay'></div> : null }
       </div>
@@ -42,7 +25,17 @@ const BoardSquare = React.createClass({
 const squareTarget = {
   drop(props, monitor) {
     const tile = monitor.getItem();
-    // TODO: Drop handling here
+
+    const tileData = {
+      location: 'board',
+      _id: tile._id,
+      x: props.x,
+      y: props.y
+    };
+
+    // Invoke the action.
+    props.placeTile(tileData);
+
   }
 }
 
