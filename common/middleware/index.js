14fd6e5dce698c056e0a91bf70b0ae618ec686c6
Joshua Comeau <joshwcomeau@gmail.com>
2015-12-13 21:39:24 -0500

    Quick update to update the logger middleware to support thunks. Logs are much more readable now.

--- common/middleware/index.js
+++ common/middleware/index.js
@@ -1,8 +1,18 @@
 export const logger = store => next => action => {
-  console.group(action.type)
-  console.info('dispatching', action)
-  let result = next(action)
-  console.log('next state', store.getState().toJS())
-  console.groupEnd(action.type)
-  return result
+  // If action is a function, instead of an object, it means this is a
+  // thunk, and it's about to dispatch further actions.
+  const isThunk   = typeof action === 'function';
+  const groupName = isThunk ? 'Thunk' : action.type;
+
+  console.group( groupName );
+
+  if ( !isThunk ) console.info('dispatching', action);
+
+  let result = next(action);
+
+  let loggedState = store.getState().toJS();
+  console.log( (isThunk ? 'Final state' : 'Next state'), loggedState );
+
+  console.groupEnd( groupName );
+  return result;
 }
