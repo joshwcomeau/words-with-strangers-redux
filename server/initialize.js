67cfdf94e764e61cfc43f3f73b78acc613a5357b
Joshua Comeau <joshwcomeau@gmail.com>
2015-12-23 10:05:42 -0500

    Added a new config file, 'defaults'. The order is now defaults -> environment -> private, with later  overwriting earlier values.

--- server/initialize.js
+++ server/initialize.js
@@ -10,21 +10,21 @@ if ( typeof process.env.NODE_ENV === 'undefined' ) {
   process.env.NODE_ENV = 'development'
 }
 
-const DEFAULT_CONFIG = `./server/config/${process.env.NODE_ENV}.json`
-
+const DEFAULT_CONFIG  = './server/config/defaults.json';
+const ENV_CONFIG      = `./server/config/${process.env.NODE_ENV}.json`
 const PRIVATE_CONFIG  = './server/config/private.json';
 
 
 // Setup nconf to use (in-order):
 //   1. Command-line arguments
 //   2. Environment variables
-//   3. Our private config (secret keys, etc)
-//   4. Our default configuration, in a json file
+//   3. Our private config (eg. secret keys)
+//   4. Our environment-specific config (eg. database info)
+//   5. Our default configuration (eg. timezone)
+
 nconf
   .argv()
   .env()
   .file('private', PRIVATE_CONFIG)
-  .file('default', DEFAULT_CONFIG);
-
-// Always use EST. Makes my life easier.
-process.env.TZ = 'EST';
+  .file('environment', ENV_CONFIG)
+  .file('defaults', DEFAULT_CONFIG);
