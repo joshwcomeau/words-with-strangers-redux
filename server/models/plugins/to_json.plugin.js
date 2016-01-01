b660c1e9658522de4758177de504d17ae598beff
Josh Comeau <josh@breather.com>
2016-01-01 14:22:56 -0500

    Finished the recursive _id -> id conversion.

--- server/models/plugins/to_json.plugin.js
+++ server/models/plugins/to_json.plugin.js
@@ -11,7 +11,7 @@ export function recursivelyConvertIds(json) {
   return _.reduce(json, (memo, val, key) => {
     // If this field is our `_id` field, take the `id` instead.
     if ( key === '_id' ) {
-      memo.id = val.id;
+      memo.id = val.toString();
     }
 
     // Don't include __v
@@ -22,6 +22,19 @@ export function recursivelyConvertIds(json) {
     // Convert any other fields holding objectIds to strings.
     else if ( val instanceof mongoose.Types.ObjectId ) {
       memo[key] = val.toString();
+    }
+
+    // If it's an array, we need to map each child to a converted version
+    else if ( _.isArray(val) ) {
+      memo[key] = val.map( valItem => {
+        if ( typeof valItem === 'object' ) return recursivelyConvertIds(valItem)
+        return valItem
+      });
+    }
+
+    // If it's an embedded document, we need to process it as well
+    else if ( typeof val === 'object' ) {
+      return recursivelyConvertIds(val);
     }
 
     // For all other values, just attach it as normal.
--- test/models/plugins/to_json.plugin.spec.js
+++ test/models/plugins/to_json.plugin.spec.js
@@ -43,25 +43,59 @@ describe('#toJSON plugin', () => {
       .then( game => {
         gameObject  = game;
         gameJson    = game.toJSON();
+        console.log("HJSON", gameJson)
         return done();
       });
   });
 
-  it('does not mutate the game itself', () => {
-    expect(gameObject._id).to.exist;
-    expect(gameObject._id).to.be.an('object');
-    expect(gameObject.createdByUserId).to.exist;
-    expect(gameObject.createdByUserId).to.be.an('object');
-    expect(gameObject.__v).to.exist;
-  })
-
-  it('converts the game\'s base _id', () => {
+  it("converts the game's base _id", () => {
     expect(gameJson._id).not.to.exist;
     expect(gameJson.id).to.exist;
     expect(gameJson.id).to.be.a('string');
   });
 
+  it("does not mutate the original game's _id", () => {
+    expect(gameObject._id).to.be.an.instanceof(mongoose.Types.ObjectId);
+  })
+
   it('converts the createdByUserId field', () => {
     expect(gameJson.createdByUserId).to.be.a('string');
+  });
+
+  it('does not mutate the original createdByUserId field', () => {
+    expect(gameObject.createdByUserId).to.be.an.instanceof(mongoose.Types.ObjectId);
+  })
+
+  it('converts the embedded player document', () => {
+    const player = gameJson.players[0];
+
+    expect(player._id).not.to.exist;
+    expect(player.id).to.be.a('string');
+  });
+
+  it('does not mutate the original embedded player document', () => {
+    const player = gameObject.players[0];
+
+    expect(player._id).to.exist;
+    expect(player._id).to.be.an.instanceof(mongoose.Types.ObjectId);
+  });
+
+  it('converts the embedded tile documents', () => {
+    const tiles = gameJson.rack;
+
+    tiles.forEach( tile => {
+      expect(tile._id).not.to.exist;
+      expect(tile.id).to.be.a('string');
+    })
+
+  });
+
+  it('does not mutate the original embedded tile documents', () => {
+    const tiles = gameObject.rack;
+
+    tiles.forEach( tile => {
+      expect(tile._id).to.exist;
+      expect(tile._id).to.be.an.instanceof(mongoose.Types.ObjectId);
+    });
   });
 });
