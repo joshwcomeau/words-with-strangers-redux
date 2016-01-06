61cca18d94dc59f7ef8273950c40bb4d23e8c776
Joshua Comeau <joshwcomeau@gmail.com>
2016-01-06 08:49:51 -0500

    Used composition to make a Layout creator, so that all the shared logic (header, flash messages) isn't duplicated across two layout files. Yay for not needing mixins

--- common/components/layout/Layout.prod.jsx
+++ common/components/layout/Layout.prod.jsx
@@ -4,33 +4,9 @@ import ReactCSSTransitionGroup  from 'react-addons-css-transition-group';
 import Header       from '../../components/layout/header/Header.jsx';
 import FlashMessage from '../../components/layout/FlashMessage.jsx';
 
+import createLayout from './createLayout';
 
-export default class Layout extends React.Component {
-  render() {
-    return (
-      <div id="app-view">
-        <Header
-          authenticated={this.props.auth.authenticated}
-          user={this.props.auth.user}
-          error={this.props.auth.error}
-          activeMenu={this.props.ui.menu}
-          openMenu={this.props.openMenu}
-          closeMenu={this.props.closeMenu}
-          login={this.props.login}
-          logout={this.props.logout}
-        />
-        <div id="main-layout-header-spacer"></div>
 
-        <ReactCSSTransitionGroup
-          transitionName="flash-message-transition"
-          transitionEnterTimeout={650}
-          transitionLeaveTimeout={450}
-        >
-          { this.props.ui.flash ? <FlashMessage message={this.props.ui.flash.message} type={this.props.ui.flash.type} /> : null }
-        </ReactCSSTransitionGroup>
+const Layout = createLayout();
 
-        {this.props.children}
-      </div>
-    );
-  }
-}
+export default Layout;
---
+++ common/components/layout/createLayout.jsx
@@ -0,0 +1,53 @@
+import React                    from 'react';
+import ReactCSSTransitionGroup  from 'react-addons-css-transition-group';
+
+import Header       from '../../components/layout/header/Header.jsx';
+import FlashMessage from '../../components/layout/FlashMessage.jsx';
+
+export default function createLayout(DevTools = null) {
+  return class Layout extends React.Component {
+    renderHeader() {
+      return (
+        <Header
+          authenticated={this.props.auth.authenticated}
+          user={this.props.auth.user}
+          error={this.props.auth.error}
+          activeMenu={this.props.ui.menu}
+          openMenu={this.props.openMenu}
+          closeMenu={this.props.closeMenu}
+          login={this.props.login}
+          logout={this.props.logout}
+        />
+      );
+    }
+
+    renderHeaderSpacer() {
+      return <div id="main-layout-header-spacer"></div>;
+    }
+
+    renderFlashMessage() {
+      return (
+        <ReactCSSTransitionGroup
+          transitionName="flash-message-transition"
+          transitionEnterTimeout={650}
+          transitionLeaveTimeout={450}
+        >
+          { this.props.ui.flash ? <FlashMessage message={this.props.ui.flash.message} type={this.props.ui.flash.type} /> : null }
+        </ReactCSSTransitionGroup>
+      );
+    }
+
+    render() {
+      return (
+        <div id="layout">
+          { this.renderHeader() }
+          { this.renderHeaderSpacer() }
+          { this.renderFlashMessage() }
+          { this.props.children }
+
+          { DevTools ? <DevTools /> : null }
+        </div>
+      );
+    }
+  }
+}
