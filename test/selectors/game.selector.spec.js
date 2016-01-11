165ec736cfd1d39dc2debfd032fc90f0a24b29c3
Joshua Comeau <joshwcomeau@gmail.com>
2016-01-11 08:43:23 -0500

    Remove selector specs
    All of the current-turn logic has been moved to the server. Removed
    failing tests for logic that no longer lives in the selector.

--- test/selectors/game.selector.spec.js
+++ test/selectors/game.selector.spec.js
@@ -3,38 +3,9 @@ import { List, Map, fromJS }  from 'immutable';
 import * as _                 from 'lodash';
 
 
-import gameSelector, {
-  calculateCurrentTurnPlayer
-} from '../../common/selectors/game.selector';
+import gameSelector from '../../common/selectors/game.selector';
 
 
 
 describe('Game Selector', () => {
-  describe('#calculateCurrentTurnPlayer', () => {
-    const creator = { name: 'Josh',    id: 4 };
-    const player2 = { name: 'Dorothy', id: 2 };
-    const player3 = { name: 'Janice',  id: 8 };
-    const players = [ creator, player2, player3 ];
-
-    it('returns the first player when there are no turns', () => {
-      const turns = [];
-      expect( calculateCurrentTurnPlayer(turns, players) ).to.equal(creator);
-    });
-
-    it('returns the second player when there is 1 turn', () => {
-      const turns = [ { word: 'BOOM'} ];
-      expect( calculateCurrentTurnPlayer(turns, players) ).to.equal(player2);
-    });
-
-    it('returns the third player when there are 5 turn', () => {
-      const turns = [
-        { word: 'I'},
-        { word: 'THINK'},
-        { word: 'THIS'},
-        { word: 'WILL'},
-        { word: 'WORK'}
-      ];
-      expect( calculateCurrentTurnPlayer(turns, players) ).to.equal(player3);
-    });
-  });
 });
