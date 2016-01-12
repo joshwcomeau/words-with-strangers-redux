c6f5a46b90b6c402399e4e10c8b4c1257d1311f7
Joshua Comeau <joshwcomeau@gmail.com>
2016-01-12 09:26:48 -0500

    Improve React component testing
    Added a library to be able to easily compare JSX with Chai.

--- test/components/game/tile.spec.js
+++ test/components/game/tile.spec.js
@@ -5,18 +5,25 @@ import { expect }         from 'chai';
 import Tile from '../../../common/components/game/Tile.jsx';
 import Controls from '../../../common/components/game/Controls.jsx';
 
+// Obtain the reference to the component before React DnD wrapping
+const OriginalTile = Tile.DecoratedComponent.DecoratedComponent;
+// Stub the React DnD connector functions with an identity function
+const identity = el => el;
+
 describe('Tile', () => {
-  it('works', () => {
-    // Obtain the reference to the component before React DnD wrapping
-    const OriginalTile = Tile.DecoratedComponent.DecoratedComponent;
-    // Stub the React DnD connector functions with an identity function
-    const identity = el => el;
+  xit('validates propTypes', () => {
+    // TODO
+    // Use sinon to stub console warnings
+    // http://stackoverflow.com/questions/26124914/how-to-test-react-proptypes-through-jest
+  });
+
+  it('renders correctly', () => {
+
 
     const renderer = createRenderer();
     renderer.render(
       <OriginalTile
         connectDragSource={identity}
-        connectDropTarget={identity}
         isMyTurn={false}
         tile={{
           letter: 'J',
@@ -34,6 +41,6 @@ describe('Tile', () => {
       </div>
     );
 
-    expect(actualElement).to.deep.equal(expectedElement);
+    expect(actualElement).to.equalJSX(expectedElement);
   });
 });
--- test/test_helper.js
+++ test/test_helper.js
@@ -4,18 +4,23 @@ import mongoose       from 'mongoose';
 import nconf          from 'nconf';
 import chai           from 'chai';
 import chaiImmutable  from 'chai-immutable';
+import equalJSX       from 'chai-equal-jsx';
 
-console.log("---- Tests starting! ----");
+
+console.info("---- Tests starting! ----");
 process.env.NODE_ENV = 'test';
 
 import '../server/initialize';
 
 chai.use(chaiImmutable);
+chai.use(equalJSX);
 
 
 // connect to and wipe the test DB
 const dbName = nconf.get('DB_NAME');
 const dbUrl  = nconf.get('DB_URL')
+console.info("---- Wiping Database ----");
 execSync(`mongo ${dbName} --eval "db.dropDatabase();"`);
 
+console.info('----- Tests Starting ----');
 mongoose.connect( dbUrl + dbName );
