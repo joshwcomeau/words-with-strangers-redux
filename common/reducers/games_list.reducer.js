ad5026ecf99f9ec97b66f6559e2776ac0da6d0cf
Joshua Comeau <joshwcomeau@gmail.com>
2016-01-23 16:18:53 -0500

    Fix bug with gamesList reducer
    Formerly, every time the interface would request the games list,
    the server would respond with the games and the reducer would .concat
    these games in, even if it already had them. THis was leading to the
    reducer having duplicate copies of the same game.
    
    Turns out the fix was super simple: Just using an OrderedSet instead
    of a List, in immutablejs. This way, duplicate additions are
    automatically ignored.

--- common/reducers/games_list.reducer.js
+++ common/reducers/games_list.reducer.js
@@ -1,12 +1,12 @@
 import * as _                 from 'lodash';
-import { List, Map, fromJS }  from 'immutable';
+import { OrderedSet, Map, fromJS }  from 'immutable';
 import {
   ADD_GAMES_TO_LIST,
   GAME_STATUS_CHANGED
 } from '../constants/actions.constants';
 
 // Initial state for the 'game_list' slice of the state.
-export const initialState = List();
+export const initialState = OrderedSet();
 
 export default function game(state = initialState, action) {
   switch (action.type) {
