19724004e98ee2f027360e8cf3f741cdeb651287
Joshua Comeau <joshwcomeau@gmail.com>
2016-01-24 20:05:40 -0500

    Add player progress bars
    When submitting words, the point total will show as a green bar
    behind the player's name. Added a transition so that it slides up
    slowly. Works great!

--- common/components/game/side_panel/Players.jsx
+++ common/components/game/side_panel/Players.jsx
@@ -1,6 +1,8 @@
 import React  from 'react';
 import * as _ from 'lodash';
 
+import { POINTS_TO_WIN } from '../../../constants/config.constants';
+
 const Players = ({players}) => (
   <div className="side-panel-players">{renderPlayers(players)}</div>
 );
@@ -10,11 +12,19 @@ function renderPlayers(players) {
   players = _.sortBy(players, 'points').reverse();
 
   return players.map( (player) => {
+    const percentage = Math.round((player.points / POINTS_TO_WIN) * 100);
+    const styles = {
+      width: percentage+"%"
+    }
+
     return (
       <div className="side-panel-player" key={player.id}>
-        <div className="avatar" style={{backgroundImage: `url('${player.profilePhoto}')`}}></div>
-        <div className="username">{player.username}</div>
-        <div className="points">{player.points}</div>
+        <div className="player-data">
+          <div className="avatar" style={{backgroundImage: `url('${player.profilePhoto}')`}}></div>
+          <div className="username">{player.username}</div>
+          <div className="points">{player.points}</div>
+        </div>
+        <div className="player-progress" style={styles}></div>
       </div>
     );
   });
