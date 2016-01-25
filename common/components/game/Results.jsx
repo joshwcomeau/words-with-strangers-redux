8db147de3505bf9d6adc55407823efd05c8b2ef6
Joshua Comeau <joshwcomeau@gmail.com>
2016-01-25 09:21:15 -0500

    Tweak CSS, add button to leave completed games.
    As a band-aid before allowing modals to be dismissed, added a
    button to return to the games list once a game finishes and the
    win/lose modal pops up.
    
    Some misc styling and cleanup as well. Rearranged the constants to
    be alphabetical, as I often try to import them in this order.

--- common/components/game/Results.jsx
+++ common/components/game/Results.jsx
@@ -1,10 +1,11 @@
-import React  from 'react';
-import _      from 'lodash';
+import React    from 'react';
+import { Link } from 'react-router';
+import _        from 'lodash';
 
 const Results = ({ isWinner }) => (
-  <div id="results">
-    <div className="results-outer">
-      <div className="results-inner">
+  <div id="results" className="modal">
+    <div className="modal-outer">
+      <div className="modal-inner">
         { isWinner ? generateWin() : generateLose() }
       </div>
     </div>
@@ -16,7 +17,8 @@ function generateWin() {
     <div className="win">
       <img src="https://s3.amazonaws.com/wordswithstrangers/win.png" />
       <h2>You win!</h2>
-      <p>You did it, you wordy son of a gun! Your vocabulary is without equal.</p>
+      <p>You did it, you wordy son of a gun! Your vocabulary is unequaled, unparalleled, unrivaled and unsurpassed.</p>
+      <Link className="button" to="/">Return to Games List</Link>
     </div>
   );
 }
@@ -27,6 +29,7 @@ function generateLose() {
       <img src="https://s3.amazonaws.com/wordswithstrangers/lose.png" />
       <h2>Dammit.</h2>
       <p>Afraid to say it, but your opponent out-worded you this time. Don't be discouraged! You'll get them next time.</p>
+      <Link className="button" to="/">Return to Games List</Link>
     </div>
   );
 }
--- common/constants/actions.constants.js
+++ common/constants/actions.constants.js
@@ -1,38 +1,38 @@
 // GAME constants
-export const SWITCH_TILE_POSITIONS  = 'SWITCH_TILE_POSITIONS';
+export const PASS_TURN              = 'PASS_TURN';
 export const PICK_TILE              = 'PICK_TILE';
 export const PLACE_TILE             = 'PLACE_TILE';
+export const RECALL_TILES_TO_RACK   = 'RECALL_TILES_TO_RACK';
+export const SHUFFLE_RACK           = 'SHUFFLE_RACK';
+export const SUBMIT_SWAPPED_TILES   = 'SUBMIT_SWAPPED_TILES';
 export const SUBMIT_WORD            = 'SUBMIT_WORD';
-export const PASS_TURN              = 'PASS_TURN';
 export const SUBSCRIBE_TO_GAME      = 'SUBSCRIBE_TO_GAME';
+export const SWITCH_TILE_POSITIONS  = 'SWITCH_TILE_POSITIONS';
+export const TOGGLE_SWAPPING        = 'TOGGLE_SWAPPING';
 export const UNSUBSCRIBE_FROM_GAME  = 'UNSUBSCRIBE_FROM_GAME';
 export const UPDATE_GAME_STATE      = 'UPDATE_GAME_STATE';
-export const SHUFFLE_RACK           = 'SHUFFLE_RACK';
-export const RECALL_TILES_TO_RACK   = 'RECALL_TILES_TO_RACK';
-export const TOGGLE_SWAPPING        = 'TOGGLE_SWAPPING';
-export const SUBMIT_SWAPPED_TILES   = 'SUBMIT_SWAPPED_TILES';
 
 // GAMES_LIST constants
-export const REQUEST_GAMES_LIST     = 'REQUEST_GAMES_LIST';
 export const ADD_GAMES_TO_LIST      = 'ADD_GAMES_TO_LIST';
-export const JOIN_GAME              = 'JOIN_GAME';
 export const CREATE_GAME            = 'CREATE_GAME';
 export const GAME_STATUS_CHANGED    = 'GAME_STATUS_CHANGED';
+export const JOIN_GAME              = 'JOIN_GAME';
+export const REQUEST_GAMES_LIST     = 'REQUEST_GAMES_LIST';
 
 // AUTH constants
-export const AUTHENTICATION_SUCCESS = 'AUTHENTICATION_SUCCESS';
 export const AUTHENTICATION_FAILURE = 'AUTHENTICATION_FAILURE';
-export const ENABLE_REGISTRATION    = 'ENABLE_REGISTRATION';
-export const DISABLE_REGISTRATION   = 'DISABLE_REGISTRATION';
-export const BEGIN_REGISTRATION     = 'BEGIN_REGISTRATION';
+export const AUTHENTICATION_SUCCESS = 'AUTHENTICATION_SUCCESS';
 export const BEGIN_LOGIN            = 'BEGIN_LOGIN';
+export const BEGIN_REGISTRATION     = 'BEGIN_REGISTRATION';
+export const DISABLE_REGISTRATION   = 'DISABLE_REGISTRATION';
+export const ENABLE_REGISTRATION    = 'ENABLE_REGISTRATION';
 export const LOGOUT                 = 'LOGOUT';
 
 // UI constants
-export const OPEN_MENU              = 'OPEN_MENU';
 export const CLOSE_MENU             = 'CLOSE_MENU';
-export const SET_AND_DISPLAY_FLASH  = 'SET_AND_DISPLAY_FLASH';
 export const DISMISS_FLASH          = 'DISMISS_FLASH';
+export const OPEN_MENU              = 'OPEN_MENU';
+export const SET_AND_DISPLAY_FLASH  = 'SET_AND_DISPLAY_FLASH';
 
 // ROUTER constants
 export const PUSH_PATH              = 'PUSH_PATH';
